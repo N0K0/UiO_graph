419afc66352142369b62ed7325e7c43acb18c42a
Nikolas <nikolasp@ifi.uio.no>
2016-06-19 23:12:43 +0200

    eh. More stuff

--- graph.py
+++ graph.py
@@ -1,22 +1,36 @@
 class Graph:
-    _list_of_nodes = []
+    _dict_of_nodes = {}
 
     def add_node(self,node):
-        self._list_of_nodes.append(node)
+        self._dict_of_nodes[node.get_code()] = node
+
+    def get_node(self,code):
+        return self._dict_of_nodes.get(code)
+
+    def __len__(self):
+        return len(self._dict_of_nodes)
+
+    def get_graph(self):
+        return self._dict_of_nodes
 
 
 class Node:
     _name = ''
     _code = ''
     _site = ''
+    _pre_req = ''
 
     _related_nodes = []
     _related_nodes_reverse = []
 
-    def __init__(self, name, code, site):
-        _name = name
-        _code = code
-        _site = site
+    def __init__(self, name, code, site,pre_req):
+        self._name = name
+        self._code = code
+        self._site = site
+        self._pre_req = pre_req
+
+    def get_code(self):
+        return self._code
 
     def get_name(self):
         return self._name
@@ -24,13 +38,21 @@ class Node:
     def get_site(self):
         return self._site
 
+    def get_pre_req(self):
+        return self._pre_req
+
     def get_related(self):
-        return self._related_subjects
+        return self._related_nodes
 
     def add_related_node(self,node):
+        if node is None:
+            return
         self._related_nodes.append(node)
         node.add_related_node_rev(self)
 
     def add_related_node_rev(self,node):
         self._related_nodes_reverse.append(node)
+
+    def __str__(self):
+        return str(self._code)
 
--- parser.py
+++ parser.py
@@ -1,8 +1,13 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import requests
 from bs4 import BeautifulSoup
 import re
 
 import graph
+
+import time
 
 url_subjects = 'http://www.uio.no/studier/emner/'
 url_root = 'http://www.uio.no/'
@@ -14,51 +19,99 @@ session = requests.session()
 subjects = session.get(url_subjects)
 soup = BeautifulSoup(subjects.text,'html.parser')
 
-subject_graph = graph.Graph
+subject_graph = graph.Graph()
 
-for link in soup.find_all('h3'):
-    print link
-    if 'Andre emner' in link:
-        continue
-    link_res = re.search(r'"(\w.*?/)"',str(link))
-    page_queue.append(url_subjects+link_res.group(0).replace('"',''))
+def main():
+    get_faculty()
+    print page_queue
+    get_subjects()
+    parse_course_sites()
+    build_dependencies()
 
-
-print page_queue
-for page in page_queue:
-    print page
-    for page_num in range(1,2):
-        print 'Getting page {}'.format(page_num)
-        subject_page = requests.get(page,params={'page':str(page_num)})
-        temp_soup = BeautifulSoup(subject_page.text,'html.parser')
-        check = temp_soup.find(id='vrtx-listing-filter-no-results')
-        if check: #Breaks the loop
-            print 'Last page reached. Continuing to next faculty'
-            break
-        else: #This is where is gather the subjects
-            subjects = temp_soup.find_all(href=re.compile(r'/studier/emner/.*?/index.html'))
-            for link in subjects:
-                link_res = re.search(r'"(.*?)"', str(link)).group(0).replace('"','')
-                #print url_root + link_res
-                work_queue.append(url_root + link_res)
-
-#vrtx-listing-filter-no-results
-
-print 'Found %s subjects' % len(work_queue)
-
-for course_site in work_queue:
-    print "Parsing subject %s" % course_site
-    temp_course = requests.get(course_site)
-    soup = BeautifulSoup(temp_course.text, 'html.parser')
-
-    pre_req = soup.find(id='prerequisites')
-
-    name = soup.find(id='hdr')
-    blurb = soup.find(id='course-content')
-
-    print 'lol'
+    return 0
 
 
+def get_faculty():
 
-    print '\n' + '-'*20 + '\n'
+    for link in soup.find_all('h3'):
+        print link
+        if 'Andre emner' in link:
+            continue
+        link_res = re.search(r'"(\w.*?/)"',str(link))
+        page_queue.append(url_subjects+link_res.group(0).replace('"',''))
 
+
+def get_subjects():
+    for page in page_queue:
+        print page
+        for page_num in range(1,2):
+            print 'Getting page {}'.format(page_num)
+            subject_page = requests.get(page,params={'page':str(page_num)})
+            temp_soup = BeautifulSoup(subject_page.text.encode('utf-8'),'html.parser')
+            check = temp_soup.find(id='vrtx-listing-filter-no-results')
+            if check: #Breaks the loop
+                print 'Last page reached. Continuing to next faculty'
+                break
+            else: #This is where is gather the subjects
+                subjects = temp_soup.find_all(href=re.compile(r'/studier/emner/.*?/index.html'))
+                for link in subjects:
+                    link_res = re.search(r'"(.*?)"', str(link)).group(0).replace('"','')
+                    #print url_root + link_res
+                    work_queue.append(url_root + link_res)
+
+    #vrtx-listing-filter-no-results
+    print 'Found %s subjects' % len(work_queue)
+
+
+def parse_course_sites():
+
+    for course_site in work_queue:
+
+        if not '1010' in course_site:
+            continue
+        #print "Parsing subject %s" % course_site
+        temp_course = requests.get(course_site)
+        soup = BeautifulSoup(temp_course.text, 'html.parser')
+
+        pre_req = soup.find(id='prerequisites')
+        if pre_req is not None:
+            if pre_req.findChild('ul'):
+                pre_req = pre_req.findChild('ul').text.encode('utf-8')
+                #print "Pre req: {}".format(pre_req)
+            elif pre_req.findChild('p'):
+                pre_req = pre_req.findChild('p').text.encode('utf-8')
+                #print "Pre req: {}".format(pre_req)
+
+
+        name = soup.find(id='vrtx-course-title-toc')
+
+        name = name.findChild('h1')
+        code = name.text.encode('utf-8').split('-')[0]
+        print code
+
+        blurb = soup.find(id='course-content')
+        if blurb:
+            blurb = blurb.findChild('p')
+
+        subject_graph.add_node(graph.Node(name.text.encode('utf-8'),code,course_site,pre_req))
+
+        #print '\n' + '-'*20 + '\n'
+
+def build_dependencies():
+
+    for subjects in subject_graph.get_graph().values():
+        pre_req = parse_pre_req(subjects)
+        for req_subs in pre_req:
+            subjects.add_related_node(subject_graph.get_node(req_subs))
+
+
+def parse_pre_req(subject):
+    results = None
+    p = re.compile(ur'([A-Z0-9]{3,})',re.UNICODE)
+    text = subject.get_pre_req()
+    if text:
+        results = p.findall(text)
+
+    return results
+
+main()
